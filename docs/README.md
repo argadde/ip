7a79f59e5fc5eccedd27245981aa407ab62e625b
argadde <argadde@gmail.com>
2025-08-19 17:46:34 +0800

    Update Duke to Bevo with Rename, Greet, and Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bevo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bevo.java
@@ -0,0 +1,14 @@
+public class Bevo {
+    public static void main(String[] args) {
+        String logo = "|||||   -----   \\     /   -----\n"
+                + "|    |  |        \\   /   |     |\n"
+                + "|||||   ----      \\ /    |     |\n"
+                + "|    |  |          |     |     |\n"
+                + "|||||   -----      |      ----- \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Bevo.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
