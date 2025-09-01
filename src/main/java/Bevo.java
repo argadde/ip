38099f3332a11da297c5c2fa6568112f8be5e3f2
argadde <argadde@gmail.com>
2025-09-01 17:12:37 +0800

    Implement command echoing feature

--- src/main/java/Bevo.java
+++ src/main/java/Bevo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bevo {
     public static void main(String[] args) {
         String logo = "|||||   -----   \\     /   -----\n"
@@ -7,8 +9,28 @@ public class Bevo {
                 + "|||||   -----      |      ----- \n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello! I'm Bevo.");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\t_____________________________________________________");
+        System.out.println("\t  Hello! I'm Bevo.");
+        System.out.println("\t  What can I do for you?");
+        System.out.println("\t_____________________________________________________\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.toLowerCase().equals("bye")) {
+                System.out.println("\t_____________________________________________________");
+                System.out.println("\t  Bye. Hope to see you again soon!");
+                System.out.println("\t_____________________________________________________");
+                break;
+            }
+
+            System.out.println("\t_____________________________________________________");
+            System.out.println("\t  " + input);
+            System.out.println("\t_____________________________________________________\n");
+        }
+
+        scanner.close();
     }
 }
